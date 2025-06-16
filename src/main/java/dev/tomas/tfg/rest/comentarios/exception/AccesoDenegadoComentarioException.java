aef9ee5212dda7e92d1ec60382ea39ed1b2fcb0f
Tomas Vaquerin Jimenez <vaquerinjimenez@gmail.com>
2025-06-16 17:39:36 +0200

    Comentario Error

--- src/main/java/dev/tomas/tfg/rest/comentarios/exception/AccesoDenegadoComentarioException.java
+++ src/main/java/dev/tomas/tfg/rest/comentarios/exception/AccesoDenegadoComentarioException.java
@@ -3,8 +3,6 @@ package dev.tomas.tfg.rest.comentarios.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.UUID;
-
 @ResponseStatus(HttpStatus.FORBIDDEN)
 public class AccesoDenegadoComentarioException extends ComentariosException {
 
